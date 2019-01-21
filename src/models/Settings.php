1a3f378fe1bb9fbb1976ed103e33a06a112733eb
Luke Bares <luke@nehemedia.de>
2019-01-21 18:56:10 +0100

    Default Settings
    Loads default settings with bootstrap styles.

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -10,6 +10,7 @@
 
 namespace lucasbares\craftredactorbuttons\models;
 
+use Craft;
 use craft\base\Model;
 
 /**
@@ -33,8 +34,23 @@ class Settings extends Model
     // =========================================================================
 
     public function init(){
+
+        // load default bootstrap styles if no styles are specified
         if(trim($this->buttonCss) === ''){
             $this->buttonCss = file_get_contents(__DIR__.'/../templates/defaultButtonStyles.css');
+        }
+
+        if(!count($this->buttonStyles)){
+            $this->buttonStyles = [
+                'primary' => ['label' => Craft::t('craft-redactor-buttons', 'Primary'), 'handle' => 'primary', 'cssclass' => 'btn-primary'],
+                'secondary' => ['label' => Craft::t('craft-redactor-buttons', 'Secondary'), 'handle' => 'secondary', 'cssclass' => 'btn-secondary'],
+                'success' => ['label' => Craft::t('craft-redactor-buttons', 'Success'), 'handle' => 'success', 'cssclass' => 'btn-success'],
+                'info' => ['label' => Craft::t('craft-redactor-buttons', 'Info'), 'handle' => 'info', 'cssclass' => 'btn-info'],
+                'warning' => ['label' => Craft::t('craft-redactor-buttons', 'Warning'), 'handle' => 'warning', 'cssclass' => 'btn-warning'],
+                'danger' => ['label' => Craft::t('craft-redactor-buttons', 'Danger'), 'handle' => 'danger', 'cssclass' => 'btn-danger'],
+                'light' => ['label' => Craft::t('craft-redactor-buttons', 'Light'), 'handle' => 'light', 'cssclass' => 'btn-light'],
+                'dark' => ['label' => Craft::t('craft-redactor-buttons', 'Dark'), 'handle' => 'dark', 'cssclass' => 'btn-dark'],
+            ];
         }
     }
 
@@ -44,7 +60,7 @@ class Settings extends Model
     public function rules()
     {
         return [
-            'buttonCss' => ['string'],
+            [['buttonCss'], 'string'],
         ];
     }
 }
